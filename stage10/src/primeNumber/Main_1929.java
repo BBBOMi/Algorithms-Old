15c511d8c7605b85d752023e8852f6152a3cfb78
bomi <a51022866@gmail.com>
2018-05-24 14:04:30 +0900

    primeNumber

---
+++ stage10/src/primeNumber/Main_1929.java
@@ -0,0 +1,35 @@
+package primeNumber;
+
+import java.util.*;
+
+public class Main_1929 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		ArrayList<Boolean> prime = new ArrayList<>();
+		
+		prime.add(0, false);
+		prime.add(1, false);
+		
+		for(int i=2; i<=n; i++) {
+			prime.add(i, true);
+		}
+
+		for(int i=2; (i*i)<=n; i++) {
+			if(prime.get(i)) {
+				for(int j=i*i; j<=n; j+=i) {
+					prime.set(j, false);
+				}
+			}
+		}
+		
+		while(m <= n) {
+			if(prime.get(m))
+				System.out.println(m);
+			m++;
+		}
+	}
+}
---
+++ stage10/src/primeNumber/Main_4948.java
@@ -0,0 +1,52 @@
+package primeNumber;
+
+import java.util.*;
+
+public class Main_4948 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> testCode = new ArrayList<>();
+		ArrayList<Boolean> prime = new ArrayList<>();
+		int n = 0;
+		int max = 0;
+		
+		while(true) {
+			n = sc.nextInt();
+			
+			if(n != 0) {
+				testCode.add(n);
+				if(max < n) max = n;
+			} else break;
+		}
+		
+		prime.add(0, false);
+		prime.add(1, false);
+		for(int i=2; i<=(max*2); i++) {
+			prime.add(i, true);
+		}
+		
+		for(int i=2; (i*i)<=(max*2); i++) {
+			if(prime.get(i)) {
+				for(int j=(i*i); j<=(max*2); j+=i) {
+					prime.set(j, false);
+				}
+			}
+		}
+		
+		for(int i=0; i<testCode.size(); i++) {
+			int count = 0;
+			int ind = testCode.get(i);
+			for(int j=ind; j<=(ind*2); j++) {
+				if(prime.get(j)) count++;
+			}
+			if(prime.get(ind)) count--;
+			testCode.set(i, count);
+		}
+		
+		for(int i=0; i<testCode.size(); i++)
+			System.out.println(testCode.get(i));
+	}
+
+}
