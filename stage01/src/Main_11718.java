ad61c446a26edd4a0b4f4be7fe35ae829097b598
bomi <a51022866@gmail.com>
2018-06-04 19:59:34 +0900

    print it just as it is

--- stage01/src/Main_11718.java
+++ stage01/src/Main_11718.java
@@ -1,3 +1,9 @@
+/* <문제> 입력 받은 대로 출력하는 프로그램을 작성하시오.
+ * <입력> 입력이 주어진다. 입력은 최대 100줄로 이루어져 있고, 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있다. 각 줄은 100글자를 넘지 않으며, 빈 줄은 주어지지 않는다.
+ * 		  또, 각 줄은 공백으로 시작하지 않고, 공백으로 끝나지 않는다.
+ * <출력> 입력받은 그대로 출력한다.
+ */
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main_11718 { // #11718
@@ -6,18 +12,17 @@ public class Main_11718 { // #11718
 		// TODO Auto-generated method stub
 		
 		Scanner input = new Scanner(System.in);
-		String[] arr = new String[100];
-		
-		for(int i=0; i<100; i++) {
+		ArrayList<String> list = new ArrayList<>();
+	
+		while(input.hasNextLine()) {
 			String str = input.nextLine();
-			
-			if(str == "")
+			if(str.isEmpty() || str.length() > 100 || str.endsWith(" ") || str.startsWith(" ") || list.size() >= 100)
 				break;
-			
-			arr[i] = str;
+			list.add(str);
 		}
 		
-		for(String s : arr)
-			System.out.println(s);
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
 	}
 }
