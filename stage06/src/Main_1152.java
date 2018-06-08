e024e3a95a98b0dcd0fbbdfc1ecef33be48a5a2a
bomi <a51022866@gmail.com>
2018-06-08 23:11:38 +0900

    countWord

--- stage06/src/Main_1152.java
+++ stage06/src/Main_1152.java
@@ -1,31 +1,26 @@
 
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main_1152 { // #1152
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char[] arr = new char[1000000];
 		Scanner input = new Scanner(System.in);
-		String c = input.nextLine();
-		int count = 1;
+		String str = input.nextLine();
+		int count = 0;
 		
-		for(int i=0; i<c.length(); i++ ) {
-			arr[i] = c.charAt(i);
+		if(str.length() > 1000000) return;
+		
+		StringTokenizer strToken = new StringTokenizer(str, " ");
+		
+		while(strToken.hasMoreTokens()) {
+			String token = strToken.nextToken();
+			count++;
 		}
-		
-		for(int i=0; i<c.length(); i++) {
-			if(arr[i] == ' ')
-				count++;
-		}
-		
-		for(char ch : arr)
-			System.out.print(ch);
 		
 		System.out.println(count);
 		
-
 	}
 
 }
