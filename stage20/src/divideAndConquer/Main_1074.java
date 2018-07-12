d25e1e4a1f1c4f5ddecb2f5384315603a6f51439
bomi <a51022866@gmail.com>
2018-07-12 16:05:25 +0900

    Z

--- stage20/src/divideAndConquer/Main_1074.java
+++ stage20/src/divideAndConquer/Main_1074.java
@@ -3,7 +3,6 @@ package divideAndConquer;
 import java.util.*;
 
 public class Main_1074 {
-	static int count;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String line = sc.nextLine().trim();
@@ -16,16 +15,29 @@ public class Main_1074 {
 		int size = (int)Math.pow(2, n);
 		if((n<1 || 15<n) || (0>r || 0>c || size-1<r || size-1<c)) return;
 		
-		visited(size, r, c);
-		System.out.println(count);
+		int result = visited(n, r, c);
+		System.out.println(result);
 	}
 	
-	public static void visited(int size, int r, int c) {
-		if(r == 0 && c == 0) return;	
-		size /= 2;
-		r /= 2;
-		c /= 2;
-		count += 4;
-		visited(size, r, c);
+	public static int log(int n ,int base) {
+		return (int)(Math.log(n) / Math.log(base));
+	}
+	
+	public static int visited(int n, int r, int c) {
+		int value = 0;
+		while(true) {
+			int r1 = (int)Math.pow(2, log(r, 2));
+			int c1 = (int)Math.pow(2, log(c, 2));		
+			r1 = (r1 < c1) ? 0 : r1;
+			c1 = (c1 < r1) ? 0 : c1;
+			int R1 = (r1 == 0) ? 0 : (int)Math.pow(2, log(r1, 2) * 2 + 1);
+			int C1 = (c1 == 0) ? 0 : (int)Math.pow(2, log(c1, 2) * 2);
+			value += R1 + C1;
+			if(r1 == r && c1 == c) return value;
+			r -= r1;
+			c -= c1;
 		}
-}
+	}
+}		
+
+
