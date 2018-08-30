124c19010a11ad2bf383bf240ba895e7fad8a02e
bomi <a51022866@gmail.com>
2018-08-30 16:26:07 +0900

    yet

--- dynamicProgramming/src/dynamicProgramming/Main_2579.java
+++ dynamicProgramming/src/dynamicProgramming/Main_2579.java
@@ -14,19 +14,19 @@ public class Main_2579 {
 		}
 		
 		int sum = score[score.length-1];
-		int count = 0; // 스템 수
+		int count = 0; // 스텝 수
 		for(int i=score.length-2; i>0; ) {
 			if(score[i] >= score[i-1] && count < 3) {
 				sum += score[i];
 				i--;
 				count++;
+				//System.out.println("1sum : " + sum + "    count: " + count + "   i : " + i );
 			} else {
 				sum += score[i-1];
 				i -= 2;
 				count = 0;
-			}
-			
-			if(count < 3)
+				//System.out.println("2sum : " + sum + "    count: " + count + "   i : " + i );
+			} 
 		}
 		
 		System.out.println(sum);
