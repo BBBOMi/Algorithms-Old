0c88529070e02704c207fca7892aa07f3f7a88d8
bomi <a51022866@gmail.com>
2018-09-18 19:00:01 +0900

    modify

--- stage14/src/fibonacci/Main_2747.java
+++ stage14/src/fibonacci/Main_2747.java
@@ -22,9 +22,13 @@ public class Main_2747 {
 	}
 	
 	public static int fibonacci(int n) {
-		if(n == 0) return 0;
-		if(n == 1) return 1;
-		return fibonacci(n-1) + fibonacci(n-2);
+		int[] fib = new int[n+1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for(int i=2; i<=n; i++) {
+			fib[i] = fib[i-1] + fib[i-2];
+		}
+		return fib[n];
 	}
 
 }
