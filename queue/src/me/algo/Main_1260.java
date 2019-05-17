6ae8dbc935fe5aba104b03d502bee50a21346c4f
bomi <a51022866@gmail.com>
2019-05-17 16:32:59 +0900

    Rename package name

---
+++ queue/src/me/algo/Main_1260.java
@@ -0,0 +1,61 @@
+package me.algo;
+
+import java.util.*;
+
+public class Main_1260 {
+	static int[][] graph;
+	static boolean[] visited;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String Line = sc.nextLine().trim();
+		String[] value = Line.split(" ");
+		
+		int n = Integer.parseInt(value[0]);
+		if(n < 0 || 1000 < n) return;
+		
+		graph = new int[n][n];
+		visited = new boolean[n];
+		
+		int m = Integer.parseInt(value[1]);
+		if(m < 0 || 10000 < m) return;
+		
+		for(int i=0; i<m; i++) {
+			int v1 = sc.nextInt();
+			int v2 = sc.nextInt();
+			
+			graph[v1-1][v2-1] = graph[v2-1][v1-1] = 1;		
+		}
+		
+		int v = Integer.parseInt(value[2]);
+		
+		DFS(v); System.out.println();
+		BFS(v);
+	}
+	
+	public static void DFS(int v) {
+		visited[v-1] = true;
+		System.out.print(v + " ");
+		
+		for(int i=0; i<graph.length; i++) {
+			if(!visited[i] && graph[v-1][i] == 1) {
+				DFS(i+1);
+			}
+		}
+	}
+	
+	public static void BFS(int v) {
+		visited[v-1] = true;
+		System.out.print(v + " ");
+		
+		for(int i=0; i<graph.length; i++) {
+			if(!visited[i] && graph[v-1][i] == 1) {
+				visited[i] = true;
+				System.out.print(i + " ");
+			}
+		}
+		
+	}
+
+}
+
