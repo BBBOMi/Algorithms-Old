b39b10936c64e328b71761e7fdea0441822f97e2
bomi <a51022866@gmail.com>
2019-05-25 15:53:39 +0900

    2019 05 25 solved

---
+++ string/src/me/algo/Main_2743b.java
@@ -0,0 +1,15 @@
+package me.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by bomi on 2019-05-25.
+ */
+public class Main_2743b {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
