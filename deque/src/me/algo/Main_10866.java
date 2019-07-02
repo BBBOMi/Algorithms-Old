c797b71b7efdbee91ebf0150341a0d87d9ebc0f4
bomi <a51022866@gmail.com>
2019-07-02 16:03:36 +0900

    2019 07 02 solved

--- deque/src/me/algo/Main_10866.java
+++ deque/src/me/algo/Main_10866.java
@@ -1,46 +1,105 @@
+/* <문제> 정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+	명령은 총 여덟 가지이다.
+	push_front X: 정수 X를 덱의 앞에 넣는다.
+	push_back X: 정수 X를 덱의 뒤에 넣는다.
+	pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+	pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+	size: 덱에 들어있는 정수의 개수를 출력한다.
+	empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+	front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+	back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ * <입력> 첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘쨰 줄부터 N개의 줄에는 명령이 하나씩 주어진다.
+ 	주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다.문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+ * <출력> 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+ */
 package me.algo;
 
-import java.io.*;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main_10866 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine().trim());
+		if(!(1 <= n && n <= 10000)) return;
+		
+		MyDeque DQ = new MyDeque();
+		
+		for(int i=0; i<n; i++) {
+			String order = sc.nextLine().trim();
+			switch(order) {
+			case "pop_front":
+				System.out.println(DQ.popFront());
+				break;
+			case "pop_back":
+				System.out.println(DQ.popBack());
+				break;
+			case "size":
+				System.out.println(DQ.getSize());
+				break;
+			case "empty":
+				System.out.println(DQ.isEmpty());
+				break;
+			case "front":
+				System.out.println(DQ.peekFront());
+				break;
+			case "back":
+				System.out.println(DQ.peekBack());
+				break;
+			default:
+				String[] ord = order.split(" ");
+				int x = Integer.parseInt(ord[1]);
+				if(!(1 <= x && x <= 100000)) return;
+				
+				if("push_front".equals(ord[0])) {
+					DQ.pushFront(x);
+				} else if("push_back".equals(ord[0])) {
+					DQ.pushBack(x);
+				}
+			}
+		}
+		
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Integer> dq = new LinkedList<>();
-        for(int i=0; i<n; i++) {
-            String[] str = br.readLine().split(" ");
-            switch(str[0]) {
-                case "push_front" :
-                    dq.offerFirst(Integer.parseInt(str[1]));
-                    break;
-                case "push_back" :
-                    dq.offerLast(Integer.parseInt(str[1]));
-                    break;
-                case "pop_front" :
-                    bw.write(dq.isEmpty() ? "-1\n" :  dq.pollFirst() + "\n");
-                    break;
-                case "pop_back" :
-                    bw.write(dq.isEmpty() ? "-1\n" : dq.pollLast() + "\n");
-                    break;
-                case "size" :
-                    bw.write(dq.size() + "\n");
-                    break;
-                case "empty" :
-                    bw.write(dq.isEmpty() ? "1\n" : "0\n");
-                    break;
-                case "front" :
-                    bw.write(dq.isEmpty() ? "-1\n" : dq.peekFirst() + "\n");
-                    break;
-                case "back" :
-                    bw.write(dq.isEmpty() ? "-1\n" : dq.peekLast() + "\n");
-                    break;
-            }
-        }
-        bw.close();
-    }
+	}
+
+}
+
+class MyDeque {
+	private Deque<Integer> myDeque;
+	
+	public MyDeque() {
+		myDeque = new LinkedList<Integer>();
+	}
+	
+	public void pushFront(int x) {
+		myDeque.addFirst(x);
+	}
+	
+	public void pushBack(int x) {
+		myDeque.addLast(x);
+	}
+	
+	public int popFront() {
+		return !myDeque.isEmpty() ? myDeque.removeFirst() : -1;
+	}
+	
+	public int popBack() {
+		return !myDeque.isEmpty() ? myDeque.removeLast() : -1;
+	}
+	
+	public int getSize() {
+		return myDeque.size();
+	}
+	
+	public int isEmpty() {
+		return myDeque.isEmpty() ? 1 : 0;
+	}
+	
+	public int peekFront() {
+		return !myDeque.isEmpty() ? myDeque.peekFirst() : -1;
+	}
+	
+	public int peekBack() {
+		return !myDeque.isEmpty() ? myDeque.peekLast() : -1;
+	}
 }
---
+++ deque/src/me/algo/Main_5430.java
@@ -0,0 +1,80 @@
+/* <문제> 두 가지 함수 R(뒤집기)과 D(버리기)가 있다.
+  	함수 R은 배열에 있는 숫자의 순서를 뒤집는 함수이고, D는 첫 번째 숫자를 버리는 함수이다. 배열이 비어있는데 D를 사용한 경우에는 에러가 발생한다.
+  	함수는 조합해서 한 번에 사용할 수 있다. 예를 들어, "AB"는 A를 수행한 다음에 바로 이어서 B를 수행하는 함수이다. 예를 들어, "RDD"는 배열을 뒤집은 다음 처음 두 숫자를 버리는 함수이다.
+  	배열의 초기값과 수행할 함수가 주어졌을 때, 최종 결과를 구하는 프로그램을 작성하시오.
+ 
+ * <입력> 첫째 줄에 테스트 케이스의 개수 T가 주어진다. T는 최대 100이다.
+  	각 테스트 케이스의 첫째 줄에는 수행할 함수 p가 주어진다. p의 길이는 1보다 크거나 같고, 100,000보다 작거나 같다.
+  	다음 줄에는 배열에 들어있는 수의 개수 n이 주어진다. (0 ≤ n ≤ 100,000)
+	다음 줄에는 [x1,...,xn]과 같은 형태로 배열에 들어있는 수가 주어진다. (1 ≤ xi ≤ 100)
+
+ * <출력> 각 테스트 케이스에 대해서, 입력으로 주어진 정수 배열에 함수를 수행한 결과를 출력한다.
+  	만약, 에러가 발생한 경우에는 error를 출력한다.
+ */
+package me.algo;
+
+import java.util.*;
+
+public class Main_5430 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = Integer.parseInt(sc.nextLine().trim());
+		if(100 < t) return;
+		
+		for(int i=0; i<t; i++) {
+			String p = sc.nextLine().trim(); // 함수
+			if(!((1 <= p.length()) && p.length() <= 1000000)) return;
+			
+			int n = Integer.parseInt(sc.nextLine().trim()); // 배열 안의 수의 개수
+			if(!((0 <= n) && (n <= 1000000))) return;
+			
+			String arr = sc.nextLine().trim();
+			
+			System.out.println(function(p, arr));
+		}
+
+	}
+	
+	public static String function(String p, String arr) {
+		String number = arr.replace("[", "").replace("]", "");
+		String[] numbers = number.split(",");
+		
+		Deque<String> deque = new LinkedList<>();
+		for(int i=0; i<numbers.length; i++) {
+			deque.addLast(numbers[i]);
+		}
+		
+		boolean frontIndex = true;
+		for(int i=0; i<p.length(); i++) {
+			char ch = p.charAt(i);
+			if(ch == 'R') {
+				frontIndex = !frontIndex;
+			} else if(ch == 'D') {
+				if(deque.isEmpty() || deque.peek().equals(""))
+					return "error";
+				else if(frontIndex){
+					deque.removeFirst();
+				} else
+					deque.removeLast();
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append('[');
+		
+		Iterator<String> it = null;
+		if(frontIndex) it = deque.iterator();
+		else  it = deque.descendingIterator();
+		
+		while(it.hasNext()) {
+			sb.append(it.next());
+			if(it.hasNext()) sb.append(",");
+		}
+		
+		sb.append(']');
+		
+		return sb.toString();
+	}
+
+}
