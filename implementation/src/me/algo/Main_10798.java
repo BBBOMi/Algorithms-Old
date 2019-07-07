c4534d79575152f6a5d35fa2ee6a251641072eb7
bomi <a51022866@gmail.com>
2019-07-07 20:10:59 +0900

    2019 07 07 solved

--- implementation/src/me/algo/Main_10798.java
+++ implementation/src/me/algo/Main_10798.java
@@ -3,8 +3,6 @@ package me.algo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by bomi on 2019-05-15.
@@ -12,22 +10,30 @@ import java.util.List;
 public class Main_10798 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] arr = new char[225];
+        char[][] arr = new char[5][15];
 
-        for(int i=0; i<5; i++) {
+        for(int i=0; i<arr.length; i++) {
             String str = br.readLine();
-            for(int j=0; j<str.length(); j++) {
-                char ch = str.charAt(i);
-                arr[(j*5)+i] = ch;
+            int len = str.length();
+            for(int j=0; j<arr[0].length; j++) {
+                if(len <= j) {
+                    arr[i][j] = '!';
+                } else {
+                    arr[i][j] = str.charAt(j);
+                }
             }
         }
 
-        int i = 0;
-        while(i < arr.length) {
-            if(arr[i] != ' ') {
-                System.out.print(arr[i]);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr[0].length; i++) {
+            for(int j=0; j<5; j++) {
+                if(arr[j][i] == '!') {
+                    continue;
+                }
+                sb.append(arr[j][i]);
             }
-            i++;
         }
+
+        System.out.println(sb.toString());
     }
 }
