07b26e396dba3a118eb2c2013567eb10991a7256
bomi <a51022866@gmail.com>
2019-07-08 15:51:33 +0900

    2019 07 08 solved

--- DP/src/DP/Main_2579.java
+++ DP/src/DP/Main_2579.java
@@ -1,35 +1,35 @@
 package DP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main_2579 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] score = new int[n];
-		
-		for(int i=0; i<score.length; i++) {
-			score[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] score = new int[n + 1];
+		int[] dp = new int[n + 1];
+
+		for(int i=1 ; i<n+1; i++) {
+			score[i] = Integer.parseInt(br.readLine());
 		}
-		
-		int sum = score[score.length-1];
-		int count = 0; // 스텝 수
-		for(int i=score.length-2; i>0; ) {
-			if(score[i] >= score[i-1] && count < 3) {
-				sum += score[i];
-				i--;
-				count++;
-				//System.out.println("1sum : " + sum + "    count: " + count + "   i : " + i );
-			} else {
-				sum += score[i-1];
-				i -= 2;
-				count = 0;
-				//System.out.println("2sum : " + sum + "    count: " + count + "   i : " + i );
-			} 
+
+		dp[1] = score[1];
+		dp[2] = max(score[1]+score[2], score[2]);
+		dp[3] = max(score[1]+score[3], score[2]+score[3]);
+
+		for(int i=4; i<=n; i++) {
+			dp[i] = max(dp[i-2]+score[i], score[i-1]+score[i]+dp[i-3]);
 		}
-		
-		System.out.println(sum);
+
+		System.out.println(dp[n]);
+
 	}
 
+	static int max(int n1, int n2) {
+		return n1 >= n2 ?  n1: n2;
+	}
 }
