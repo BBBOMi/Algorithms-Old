b89a9f2f023c3544cf7bba142ec56005891fcf33
bomi <a51022866@gmail.com>
2019-07-10 16:56:03 +0900

    2019 07 10 solved

--- DP/src/DP/Main_2156.java
+++ DP/src/DP/Main_2156.java
@@ -11,24 +11,31 @@ public class Main_2156 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n+1][2];
-        boolean[][] check = new boolean[n+1][2];
 
-        for(int i=1; i<n; i++) {
-            int w = Integer.parseInt(br.readLine());
-            for(int j=0; j<dp[0].length; j++) {
-                dp[i][j] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-//                if(i > 2) {
-//                    if(check)
-//                }
-//                if() {
-//                     dp[i][j] += w;
-//                } else {
-//
-//                }
-            }
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+
+        for(int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
+        if(n == 1) {
+            System.out.println(arr[1]);
+            return;
+        }
 
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i], dp[i-1]);
+        }
+
+        System.out.println(dp[n]);
+    }
+
+    static int max(int a, int b, int c) {
+        if(a >= b) return c > a ? c : a;
+        return c > b ? c : b;
     }
 }
