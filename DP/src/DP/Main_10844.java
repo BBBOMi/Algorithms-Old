76959d394ccaad863111f247e366abd2ddb3879f
bomi <a51022866@gmail.com>
2019-07-11 15:40:18 +0900

    2019 07 11 solved

--- DP/src/DP/Main_10844.java
+++ DP/src/DP/Main_10844.java
@@ -3,23 +3,32 @@ package DP;
 import java.io.*;
 
 public class Main_10844 {
-
+	private static final int MOD = 1000000000;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n+1];
-		
-		dp[1] = 9;
-		for(int i=2; i<dp.length; i++) {
-			dp[i] = dp[i-1] * 2 - (i-1) % 1000000000;
+		int[][] dp = new int[n+1][11];
+
+		for(int i=1; i<=9; i++) {
+			dp[1][i] = 1;
 		}
-		
-		bw.write(dp[n]);
+
+		for(int i=2; i<=n; i++) {
+			dp[i][0] = dp[i-1][1];
+			for(int j=1; j<=9; j++) {
+				dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % MOD;
+			}
+		}
+
+		long res = 0;
+		for(int i : dp[n]) {
+			res += i;
+		}
+
+		bw.write((res % MOD) + "\n");
 		bw.close();
 		br.close();
-
 	}
-
 }
