bbdfadaa91574eefb3019dad4b9e8163b0c2aee8
bomi <a51022866@gmail.com>
2019-07-15 17:07:25 +0900

    2019 07 15 solved

--- DP/src/DP/Main_2167.java
+++ DP/src/DP/Main_2167.java
@@ -1,8 +1,7 @@
 package DP;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /**
  * Created by bomi on 2019-05-05.
@@ -10,39 +9,35 @@ import java.io.InputStreamReader;
 public class Main_2167 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[][] arr = new int[n+1][m+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n+1][m+1];
 
         for(int i=1; i<=n; i++) {
-            str = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for(int j=1; j<=m; j++) {
-                arr[i][j] = Integer.parseInt(str[j-1]);
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+                dp[i][j] += Integer.parseInt(st.nextToken());
             }
         }
 
-        int k = Integer.parseInt(br.readLine());
         int i, j, x, y;
+        int k = Integer.parseInt(br.readLine());
         for(int l=0; l<k; l++) {
-            str = br.readLine().split(" ");
-            i = Integer.parseInt(str[0]);
-            j = Integer.parseInt(str[1]);
-            x = Integer.parseInt(str[2]);
-            y = Integer.parseInt(str[3]);
+            st = new StringTokenizer(br.readLine());
+            i = Integer.parseInt(st.nextToken());
+            j = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
 
-            int[][] dp = new int[n+1][m+1];
-            for(int a=i; a<=x; a++) {
-                for(int b=j; b<=y; b++) {
-                    if(a == x && b == y) {
-                        dp[a][b] = dp[a-1][b] + dp[a][b-1] + arr[a][b];
-                        //System.out.println(dp[a][b]);
-                        continue;
-                    }
-                    dp[a][b] = dp[a][b-1] + arr[a][b];
-                }
-            }
+            int result = dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1];
+            bw.write(result + "\n");
         }
+        bw.close();
+        br.close();
     }
 }
