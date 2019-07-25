ef534ff2b336981f4ac425b3fef745a50ed6f071
bomi <a51022866@gmail.com>
2019-07-25 16:38:37 +0900

    2019 07 25 solved dfs

--- bfs/src/bfs/Main_1260.java
+++ bfs/src/bfs/Main_1260.java
@@ -1,34 +1,67 @@
 package bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+
+import java.util.*;
 
 public class Main_1260 {
+	private static int[][] arr;
+	private static boolean[] visited;
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(line[0]);
-		int m = Integer.parseInt(line[1]);
-		int v = Integer.parseInt(line[2]);
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
 		
-		List<Integer> list = new LinkedList<>();
-		
-		for(int i=1; i<=n; i++) {
-			list.add(i);
-		}
-		
+		arr = new int[n+1][n+1];
+		visited = new boolean[n+1];
+
 		for(int i=0; i<m; i++) {
-			String[] edge = br.readLine().split(" ");
-			int t = Integer.parseInt(edge[0]);
-			int f = Integer.parseInt(edge[1]);
-			
-			//list.
+			st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+
+			arr[n1][n2] = arr[n2][n1] = 1;
+		}
+
+		dfs(v);
+		bw.write("\n");
+		Arrays.fill(visited, false);
+		bfs(v);
+
+		bw.close();
+		br.close();
+	}
+
+	private static void dfs(int v) throws IOException {
+		visited[v] = true;
+		bw.write(v + " ");
+
+		for(int i=1, len=arr.length; i<len; i++) {
+			if(!visited[i] && arr[v][i]==1) {
+				dfs(i);
+			}
 		}
 	}
 
+	private static void bfs(int v) throws IOException {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(v);
+		visited[v] = true;
+
+		while(!queue.isEmpty()) {
+			int temp = queue.poll();
+			bw.write(temp + " ");
+			for(int i=1, len=arr.length; i<len; i++) {
+				if(!visited[i] && arr[temp][i]==1) {
+					queue.offer(i);
+					visited[i] = true;
+				}
+			}
+		}
+	}
 }
